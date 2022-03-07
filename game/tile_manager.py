e1d19de38e3a31d07b2d62144c0299ea5b544a23
seffa_work <sameffa1@gmail.com>
2022-03-07 14:48:56 -0500

    added more tiles to 1-1

--- game/tile_manager.py
+++ game/tile_manager.py
@@ -54,6 +54,12 @@ class Tile_Manager:
         self.sprites['grass_top'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_02.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
         self.sprites['grass_top_left'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_01.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
         self.sprites['grass_top_right'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_03.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
+        self.sprites['grass_bottom_left'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_19.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
+        self.sprites['grass_bottom_right'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_21.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
+        self.sprites['grass_bottom_left_corner'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_22.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
+        self.sprites['grass_bottom_right_corner'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_24.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
+        self.sprites['grass_top_left_corner'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_04.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
+        self.sprites['grass_top_right_corner'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_06.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
         self.sprites['grass_left'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_10.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
         self.sprites['grass_right'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_12.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
         self.sprites['grass_bottom'] = pg.transform.scale(pg.image.load('assets/images/tiles/Tile_05.png').convert_alpha(), (self.TILE_SIZE, self.TILE_SIZE))
@@ -75,7 +81,7 @@ class Tile_Manager:
             x = 0
             for tile in layer:
                 # Check if the tile is a collidable
-                if tile in ['2', '3', '4', '5', '6', '7', '8']:
+                if tile in ['2', '3', '4', '5', '6', '7', '8', '9', 'a']:
                     self.tile_rects.append(
                         pg.Rect(x * self.TILE_SIZE, y * self.TILE_SIZE, self.TILE_SIZE, self.TILE_SIZE))
                 x += 1
@@ -132,16 +138,40 @@ class Tile_Manager:
                 if tile == 'a':
                     display.blit(self.sprites['grass_vertical_top'], (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
 
+                # Grass bottom right
+                if tile == 'b':
+                    display.blit(self.sprites['grass_bottom_right'], (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+
+                # Grass bottom left
+                if tile == 'c':
+                    display.blit(self.sprites['grass_bottom_left'], (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+
+                # Grass bottom left corner
+                if tile == 'd':
+                    display.blit(self.sprites['grass_bottom_left_corner'],(x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+
+                # Grass bottom right corner
+                if tile == 'e':
+                    display.blit(self.sprites['grass_bottom_right_corner'], (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+
+                # Grass top left corner
+                if tile == 'f':
+                    display.blit(self.sprites['grass_top_left_corner'], (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+
+                # Grass top right corner
+                if tile == 'g':
+                    display.blit(self.sprites['grass_top_right_corner'], (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+
                 # Oak tree
-                if tile == 'o':
+                if tile == 'l':
                     pass
 
                 # Birch tree
-                if tile == 'b':
+                if tile == 'm':
                     pass
 
                 # Pine tree
-                if tile == 'p':
+                if tile == 'n':
                     pass
 
                 x += 1
