ae4cf04aca2e7498646e9e5430ea6e24712db80d
se_home <94399429+seffa1@users.noreply.github.com>
2022-03-10 06:57:00 -0500

    adjusting 0-1 design

--- game/game.py
+++ game/game.py
@@ -5,6 +5,7 @@ from .player import Player
 from .UI import UI
 
 # TODO
+#   Level triggers assets or locations
 #   Finish the game progression loop
 #   Batch Rendering of ground for less collisions checks. Only draw tiles that are within a certain distance from the player
 #   Or add a methdod to check if a tile is within the disiable screen, then use that to determine if a tile gets drawn
